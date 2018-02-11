3b941b6194d2065abb286f0c66ea5a9eedd3bb63
Renan <renan.rgsilva@gmail.com>
2018-02-11 16:40:50 -0200

    Insert method in BalanceService replaced with calculate method

--- budget-planner/src/test/java/com/budgetplanner/balance/service/BalanceServiceUTest.java
+++ budget-planner/src/test/java/com/budgetplanner/balance/service/BalanceServiceUTest.java
@@ -1,13 +1,7 @@
 package com.budgetplanner.balance.service;
 
 import static com.budgetplanner.commontests.balance.BalanceForTestsService.validBalanceWithValidBudget;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,17 +20,10 @@ public class BalanceServiceUTest {
 	@Autowired
 	private BalanceService balanceService;
 
-	private List<BalanceDTO> balances;
-	
-	@Before
-	public void setUp() {
-		balances = new ArrayList<BalanceDTO>();
-	}
 	
 	@Test
 	public void calculateMonthlyBalanceWithValidBudget() {
 		BalanceDTO balanceDTO = balanceService.calculate(validBalanceWithValidBudget());
-		assertThat(balances.size(), equalTo(1));
 	}
 
 }
