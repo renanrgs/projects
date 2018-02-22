45c1c41327eb6fc2a1b631be88ebb76c81473370
renanrgs <renan.rgsilva@gmail.com>
2018-02-22 17:13:58 -0300

    New Spring Data Repository for BalanceDTO entity

--- budget-planner/src/main/java/com/budgetplanner/balance/repository/BalanceRepository.java
+++ budget-planner/src/main/java/com/budgetplanner/balance/repository/BalanceRepository.java
@@ -1,9 +1,10 @@
 package com.budgetplanner.balance.repository;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.budgetplanner.domain.BalanceCompositePK;
 import com.budgetplanner.domain.BalanceDTO;
 
-public interface BalanceRepository {
-
-	void insert(BalanceDTO validBalance);
+public interface BalanceRepository extends CrudRepository<BalanceDTO, BalanceCompositePK> {
 
 }
