46635f9e5409aebe495503a89d07b0cbea9bf5c0
renanrgs <renan.rgsilva@gmail.com>
2018-02-22 17:19:46 -0300

    Class name changed acoording to the business logic

--- budget-planner/src/test/java/com/budgetplanner/commontests/balance/BalanceForTests.java
+++ budget-planner/src/test/java/com/budgetplanner/commontests/balance/BalanceForTests.java
@@ -3,9 +3,12 @@ package com.budgetplanner.commontests.balance;
 import java.util.List;
 
 import com.budgetplanner.commontests.budget.BudgetForTests;
+import com.budgetplanner.commontests.financialmovement.FinancialMovementForTests;
+import com.budgetplanner.domain.BalanceDTO;
 import com.budgetplanner.domain.BudgetDTO;
+import com.budgetplanner.domain.FinancialMovementDTO;
 
-public class BalanceForTestsService {
+public class BalanceForTests {
 
 	public static BudgetDTO validBudget() {
 		return BudgetForTests.validBudget();
@@ -25,5 +28,12 @@ public class BalanceForTestsService {
 	
 	public static BudgetDTO budgetWithEmptyIncomeSubCategories() {
 		return BudgetForTests.budgetWithEmptyIncomeSubCategories();
+	}
+
+	public static BalanceDTO validBalance() {
+		BudgetDTO budget = BudgetForTests.validBudget();
+		FinancialMovementDTO financialMovement = FinancialMovementForTests.validFinancialMovement();
+		BalanceDTO balance = new BalanceDTO(budget , financialMovement);
+		return balance;
 	}
 }
